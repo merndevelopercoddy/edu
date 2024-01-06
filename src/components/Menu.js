033e25920860a3685ae16bee04305ad8c1fb3875
merndevelopercoddy <mern21.01.2022@gmail.com>
2024-01-06 19:40:29 +0500

    yangi

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,7 +15,35 @@ export default function Menyu(){
       id:"3",
       manzil: "/courses",
       nomi: "Kurslar"
-    }
+    },
+    {
+      id:"4",
+      manzil:"#",
+      nomi:"Kurslar",
+      submenu:[
+        {
+          manzil:"/react",
+          nomi:"React developer"
+        },
+        {
+          manzil:"/nodejs",
+          nomi:"Nodejs developer"
+        },
+        {
+          manzil:"/vue",
+          nomi:"Vuejs developer"
+        },
+        {
+          manzil:"/fullstack",
+          nomi:"FullStack developer"
+        }
+      ]
+    },
+    {
+      id:"5",
+      manzil: "/contact",
+      nomi: "Bog'lanish"
+    },
 ];
     return (
   <header id="header" className="fixed-top">
@@ -25,36 +53,12 @@ export default function Menyu(){
 
       <nav id="navbar" className="navbar order-last order-lg-0">
         <ul>
+          {console.log(qiymat)}
           {
           qiymat.map(
-            (e)=>(<Menuitem key={e.id} {...e} />)
+            (e)=>(<Menuitem key={e.id} items={e} />)
             )
             }
-          {/* <li><a className="active" href="/index.html">Home</a></li>
-          <li><a href="/">About</a></li>
-          <li><a href="/">Courses</a></li>
-          <li><a href="/">Trainers</a></li>
-          <li><a href="/">Events</a></li>
-          <li><a href="/">Pricing</a></li> */}
-
-          <li className="dropdown"><a href="/"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="/">Drop Down 1</a></li>
-              <li className="dropdown"><a href="/"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="/">Deep Drop Down 1</a></li>
-                  <li><a href="/">Deep Drop Down 2</a></li>
-                  <li><a href="/">Deep Drop Down 3</a></li>
-                  <li><a href="/">Deep Drop Down 4</a></li>
-                  <li><a href="/">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="/">Drop Down 2</a></li>
-              <li><a href="/">Drop Down 3</a></li>
-              <li><a href="/">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a href="contact.html">Contact</a></li>
         </ul>
         <i className="bi bi-list mobile-nav-toggle"></i>
       </nav>
--- src/components/Menuitem.js
+++ src/components/Menuitem.js
@@ -1,10 +1,30 @@
-export default function Menuitem(props){
-    const { manzil , nomi} = props;
+export default function Menuitem({items}){
     return(
-        <li>
-            <a className="active" href={manzil}>
-                {nomi}
+        <>
+        {
+            items.submenu ? (
+                <>
+                <li className="dropdown"><a href="/"><span>{items.submenu.nomi}</span> <i className="bi bi-chevron-down"></i></a>
+<ul>
+    {items.submenu.map((e)=>(
+        <li><a href={e.manzil}>{e.nomi}</a></li>
+    ))}
+</ul>
+</li>
+</>
+            )
+            :
+            (
+                <li>
+            <a className="active" href={items.manzil}>
+                {items.nomi}
             </a>
         </li>
+            )
+        }
+        
+
+        </>
     )
+
 }
